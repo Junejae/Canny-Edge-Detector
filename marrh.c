7e02d60b316e2fc408785a556a40db29edd78e2a
Junejae <55151385+Junejae@users.noreply.github.com>
2020-09-07 00:33:51 -0400

    Update marrh.c

--- marrh.c
+++ marrh.c
@@ -8,24 +8,23 @@
 
 #define  PICSIZE 256
 #define  MAXMASK 100
+//#define  M_PI 3.14159265358979323846
 
          int    pic[PICSIZE][PICSIZE];
-         double outpic1[PICSIZE][PICSIZE];
-         double outpic2[PICSIZE][PICSIZE];
-         double outpicx[PICSIZE][PICSIZE];
-         double outpicy[PICSIZE][PICSIZE];
+         double xconv[PICSIZE][PICSIZE];
+         double yconv[PICSIZE][PICSIZE];
          int    edgeflag[PICSIZE][PICSIZE];
-         double mask[MAXMASK][MAXMASK]; // need to be doubled for x and y
+         int    peak[PICSIZE][PICSIZE];
          double maskx[MAXMASK][MAXMASK];
          double masky[MAXMASK][MAXMASK];
-         double conv[PICSIZE][PICSIZE];
+         double mag[PICSIZE][PICSIZE];
 
 int main(argc,argv)
 int argc;
 char **argv;
 {
         int     i,j,p,q,mr,centx,centy;
-        double  maskval,sumx,sumy,sig,maxival;
+        double  maskval,sumx,sumy,sig,maxival,slope;
         FILE    *fo1, *fo2, *fo3, *fp1, *fopen();
         char    *foobar;
 
@@ -37,11 +36,9 @@ char **argv;
         foobar = *argv;
         fo1=fopen(foobar,"wb"); // for magnitude image
 
-        /* 
         argc--; argv++;
         foobar = *argv;
         fo2=fopen(foobar,"wb"); // for peak image
-         */
 
         /* 
         argc--; argv++;
@@ -74,10 +71,10 @@ char **argv;
         for (p=-mr;p<=mr;p++)
         {  for (q=-mr;q<=mr;q++)
            {
-              maskval = p*(exp(-1*(((p*p)+(q*q))/(2*(sig*sig)))));
+              maskval = q*(exp(-1*(((p*p)+(q*q))/(2*(sig*sig)))));
               maskx[p+centy][q+centx] = maskval;
 
-              maskval = q*(exp(-1*(((p*p)+(q*q))/(2*(sig*sig)))));
+              maskval = p*(exp(-1*(((p*p)+(q*q))/(2*(sig*sig)))));
               masky[p+centy][q+centx] = maskval;
            }
         }
@@ -98,40 +95,96 @@ char **argv;
                    sumy += pic[i+p][j+q] * masky[p+centy][q+centx];
                 }
              }
-             outpicx[i][j] = sumx;
-             outpicy[i][j] = sumy;
+             xconv[i][j] = sumx;
+             yconv[i][j] = sumy;
           }
         }
 
         // bring the sqrt code from sobel.c to calculat the magnitude
-        maxival = 0;
+        maxival = 0.0;
         for (i=mr;i<256-mr;i++)
         { for (j=mr;j<256-mr;j++)
           {
-             conv[i][j]=sqrt((double)((outpicx[i][j]*outpicx[i][j]) +
-                                      (outpicy[i][j]*outpicy[i][j])));
-             if (conv[i][j] > maxival)
-                maxival = conv[i][j];
+             mag[i][j]=sqrt((double)((xconv[i][j]*xconv[i][j]) +
+                                      (yconv[i][j]*yconv[i][j])));
+             if (mag[i][j] > maxival)
+                maxival = mag[i][j];
            }
         }
 
         // Add header to see it on image viewer
         fprintf(fo1, "P5\n");
         fprintf(fo1, "%d %d\n", PICSIZE, PICSIZE);
-        fprintf(fo1, "%d\n", PICSIZE-1);
+        fprintf(fo1, "%d\n", 255);
+
+        fprintf(fo2, "P5\n");
+        fprintf(fo2, "%d %d\n", PICSIZE, PICSIZE);
+        fprintf(fo2, "%d\n", 255);
 
         printf("Producing magnitude image...\n");
         // produce mag image
         for (i=0;i<256;i++)
         { for (j=0;j<256;j++)
           {
-            conv[i][j] = (conv[i][j] / maxival) * 255;
-            fprintf(fo1,"%c",(char)((int)(conv[i][j])));
+            mag[i][j] = (mag[i][j] / maxival) * 255;
+            fprintf(fo1,"%c",(char)((int)(mag[i][j])));
           }
         }
 
+        printf("Producing peak image...\n");
+
+        // produce peak image
+        
+        for(i=mr;i<256-mr;i++){
+          for(j=mr;j<256-mr;j++){
+            
+              if((xconv[i][j]) == 0.0) xconv[i][j] = .00001;
+
+              slope = yconv[i][j]/xconv[i][j];
+
+              if( (slope <= .4142)&&(slope > -.4142))
+              {
+                if((mag[i][j] > mag[i][j-1])&&(mag[i][j] > mag[i][j+1]))
+                {
+                  peak[i][j] = 255;
+                }
+              }
+              else if( (slope <= 2.4142)&&(slope > .4142))
+              {
+                if((mag[i][j] > mag[i-1][j-1])&&(mag[i][j] > mag[i+1][j+1]))
+                {
+                  peak[i][j] = 255;
+                }
+              }
+              else if( (slope <= -.4142)&&(slope > -2.4142))
+              {
+                if((mag[i][j] > mag[i+1][j-1])&&(mag[i][j] > mag[i-1][j+1]))
+                {
+                  peak[i][j] = 255;
+                }
+              }
+              else
+              {
+                if((mag[i][j] > mag[i-1][j])&&(mag[i][j] > mag[i+1][j]))
+                {
+                  peak[i][j] = 255;
+                }
+              }
+            }
+        }
+
+        for (i=0;i<256;i++)
+        { for (j=0;j<256;j++)
+          {
+            fprintf(fo2,"%c",(char) ((peak[i][j] == 255) ? 255 : 0));
+          }
+        }
+
+
+        // close files
         fclose(fp1);
         fclose(fo1);
+        fclose(fo2);
         
         return 0;
 }
