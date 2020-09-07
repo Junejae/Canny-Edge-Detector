f7a7ee08208c54537f016d2f43d82fed9f23c74a
Junejae <55151385+Junejae@users.noreply.github.com>
2020-09-06 21:06:13 -0400

    Update marrh.c

--- marrh.c
+++ marrh.c
@@ -1,168 +1,138 @@
+// Original code is from Dr. Niels Da Vitoria Lobo
+// Modified by Junejae Kim
+// 9/06/2020
+
 #include <stdio.h>                  /*  Marr-Hildreth.c  (or marrh.c) */
 #include <math.h>
+#include <stdlib.h> // added to use atoi()
+
 #define  PICSIZE 256
 #define  MAXMASK 100
 
          int    pic[PICSIZE][PICSIZE];
          double outpic1[PICSIZE][PICSIZE];
          double outpic2[PICSIZE][PICSIZE];
+         double outpicx[PICSIZE][PICSIZE];
+         double outpicy[PICSIZE][PICSIZE];
          int    edgeflag[PICSIZE][PICSIZE];
          double mask[MAXMASK][MAXMASK]; // need to be doubled for x and y
+         double maskx[MAXMASK][MAXMASK];
+         double masky[MAXMASK][MAXMASK];
          double conv[PICSIZE][PICSIZE];
 
-main(argc,argv)
+int main(argc,argv)
 int argc;
 char **argv;
 {
-        int     i,j,p,q,s,t,mr,centx,centy;
-        double  maskval,sum,sig,maxival,minival,maxval,ZEROTOL;
-        FILE    *fo1, *fo2,*fp1, *fopen();
+        int     i,j,p,q,mr,centx,centy;
+        double  maskval,sumx,sumy,sig,maxival;
+        FILE    *fo1, *fo2, *fo3, *fp1, *fopen();
         char    *foobar;
 
         argc--; argv++;
         foobar = *argv;
-        fp1=fopen(foobar,"rb");
+        fp1=fopen(foobar,"rb"); // for original image
 
         argc--; argv++;
         foobar = *argv;
-        fo1=fopen(foobar,"wb");
+        fo1=fopen(foobar,"wb"); // for magnitude image
 
+        /* 
         argc--; argv++;
         foobar = *argv;
-        fo2=fopen(foobar,"wb");
+        fo2=fopen(foobar,"wb"); // for peak image
+         */
 
+        /* 
         argc--; argv++;
         foobar = *argv;
-        sig = atof(foobar);
+        fo2=fopen(foobar,"wb"); // for final image
+         */
 
-        argc--; argv++;
-        foobar = *argv;
-        ZEROTOL = atof(foobar);
+        sig = 1.0; // sig should be 1 in this assignment
 
         mr = (int)(sig * 3);
         centx = (MAXMASK / 2);
         centy = (MAXMASK / 2);
 
+        // Load the image
+        for (i=0;i<15;i++) // cut off the header
+          getc(fp1);
+        
         for (i=0;i<256;i++)
         { for (j=0;j<256;j++)
                 {
                   pic[i][j]  =  getc (fp1);
+                  pic[i][j]  &= 0377; // clean the data with 1111 1111
                 }
         }
 
+        printf("Gaussian calculation...\n");
         // change derivative to first derivative
         // also, double it for x and y mask
+        // done
         for (p=-mr;p<=mr;p++)
         {  for (q=-mr;q<=mr;q++)
            {
-              maskval = ((2-(((p*p)+(q*q))/(sig*sig)))*
-                      (exp(-1*(((p*p)+(q*q))/(2*(sig*sig))))));
-              (mask[p+centy][q+centx]) = maskval;
+              maskval = p*(exp(-1*(((p*p)+(q*q))/(2*(sig*sig)))));
+              maskx[p+centy][q+centx] = maskval;
+
+              maskval = q*(exp(-1*(((p*p)+(q*q))/(2*(sig*sig)))));
+              masky[p+centy][q+centx] = maskval;
            }
         }
         
+        printf("Convolution...\n");
         // also, double it up for x and y mask
+        // done
         for (i=mr;i<=255-mr;i++)
         { for (j=mr;j<=255-mr;j++)
           {
-             sum = 0;
+             sumx = 0;
+             sumy = 0;
              for (p=-mr;p<=mr;p++)
              {
                 for (q=-mr;q<=mr;q++)
                 {
-                   sum += pic[i+p][j+q] * mask[p+centy][q+centx];
+                   sumx += pic[i+p][j+q] * maskx[p+centy][q+centx];
+                   sumy += pic[i+p][j+q] * masky[p+centy][q+centx];
                 }
              }
-             outpic1[i][j] = sum;
-             conv[i][j] = sum;
+             outpicx[i][j] = sumx;
+             outpicy[i][j] = sumy;
           }
         }
 
         // bring the sqrt code from sobel.c to calculat the magnitude
-
-        maxval  = 0;
         maxival = 0;
-        minival = 255;
-
         for (i=mr;i<256-mr;i++)
         { for (j=mr;j<256-mr;j++)
           {
-             if (outpic1[i][j] > maxival)
-                maxival = outpic1[i][j];
-             if (outpic1[i][j] < minival)
-                minival = outpic1[i][j];
+             conv[i][j]=sqrt((double)((outpicx[i][j]*outpicx[i][j]) +
+                                      (outpicy[i][j]*outpicy[i][j])));
+             if (conv[i][j] > maxival)
+                maxival = conv[i][j];
            }
         }
-        if (fabs(maxival) > fabs(minival))
-           maxval = fabs(maxival);
-        else
-           maxval = fabs(minival);
 
+        // Add header to see it on image viewer
+        fprintf(fo1, "P5\n");
+        fprintf(fo1, "%d %d\n", PICSIZE, PICSIZE);
+        fprintf(fo1, "%d\n", PICSIZE-1);
+
+        printf("Producing magnitude image...\n");
+        // produce mag image
         for (i=0;i<256;i++)
         { for (j=0;j<256;j++)
           {
-             outpic1[i][j] = ((((outpic1[i][j]) / maxval) + 1) * 127);
-             fprintf(fo1,"%c",(char)((int)(outpic1[i][j])));
+            conv[i][j] = (conv[i][j] / maxival) * 255;
+            fprintf(fo1,"%c",(char)((int)(conv[i][j])));
           }
         }
 
-        for (i=mr;i<=255-mr;i++)
-        {  for (j=mr;j<=255-mr;j++)
-           {
-                         outpic2[i][j] = 0;
-             if (conv[i][j] > ZEROTOL)
-             {
-               for (p=-1;p<=1;p++)
-               {
-                 for (q=-1;q<=1;q++)
-                 {
-                   if (conv[i+p][j+q] < -ZEROTOL)
-                   {
-                     outpic2[i][j] = 255;
-                   }
-                 }
-               }
-             }
-             else if ((fabs)(conv[i][j]) < ZEROTOL)
-             {
-                     if (((conv[i+1][j] > ZEROTOL) &&
-                          (conv[i-1][j] < -ZEROTOL))   ||
-                         ((conv[i+1][j] < -ZEROTOL) &&
-                          (conv[i-1][j] > ZEROTOL)))
-                     {
-                       outpic2[i][j] = 255;
-                     }
-                     else if (((conv[i][j+1] > ZEROTOL) &&
-                               (conv[i][j-1] < -ZEROTOL))   ||
-                              ((conv[i][j+1] < -ZEROTOL) &&
-                               (conv[i][j-1] > ZEROTOL)))
-                     {
-                       outpic2[i][j] = 255;
-                     }
-                     else if (((conv[i+1][j+1] > ZEROTOL) &&
-                               (conv[i-1][j-1] < -ZEROTOL))   ||
-                              ((conv[i+1][j+1] < -ZEROTOL) &&
-                               (conv[i-1][j-1] > ZEROTOL)))
-                     {
-                       outpic2[i][j] = 255;
-                     }
-                     else if (((conv[i+1][j-1] > ZEROTOL) &&
-                               (conv[i-1][j+1] < -ZEROTOL))   ||
-                              ((conv[i+1][j-1] < -ZEROTOL) &&
-                               (conv[i-1][j+1] > ZEROTOL)))
-                     {
-                       outpic2[i][j] = 255;
-                     }
-             }
-           }
-        }
-
-        for (i=0;i<256;i++)
-        { for (j=0;j<256;j++)
-          {  if (outpic2[i][j] == 255) outpic2[i][j]=0;
-             else outpic2[i][j]=255;
-             fprintf(fo2,"%c",(char)((int)(outpic2[i][j])));
-          }
-        }
+        fclose(fp1);
+        fclose(fo1);
+        
+        return 0;
 }
 
